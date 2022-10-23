a980ff77001f97ab5a37bbdc121ef74b3faa8b24
Mike Wright <m.wright92@icloud.com>
2022-10-22 17:49:04 -0700

    set base classes of player and adventure

--- script.py
+++ script.py
@@ -4,3 +4,39 @@
 class Player:
   def __init__(self, name):
     self.name = name
+    self.decisions = {}
+    self.current_scene = 0
+  
+  def __repr__(self):
+    return "This is {name}, they have made these decisions {decisions}".format(name=self.name, decisions = self.decisions)
+  
+  def make_decision(self, decision):
+    self.decisions[self.current_scene] = decision
+    self.current_scene += 1
+
+class Adventure:
+  def __init__(self, player):
+    self.player = player
+    self.scenes = 5
+    self.current_scene = 0
+  
+  def play(self):
+    while self.current_scene < self.scenes:
+      self.scenes[self.current_scene].play(self.player)
+      self.current_scene += 1
+    if self.current_scene == self.scenes:
+      self.end_game(self.player.decisions)
+
+  def end_game(self, decisions):
+    if decisions == {0: "A", 1: "A", 2: "A", 3: "A", 4: "A"}:
+      print("You win!")
+    elif decisions == {0: "A", 1: "A", 2: "A", 3: "B", 4: "A"}:
+      print("You win!")
+    else:
+      print("You lose!")
+
+# mike = Player("Mike")
+# mike.make_decision("left")
+# mike.make_decision("right")
+
+# print(mike)
