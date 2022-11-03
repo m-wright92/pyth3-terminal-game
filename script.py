c648f24db31d707163d39ea4eaf2e25115526b50
Mike Wright <m.wright92@icloud.com>
2022-11-03 10:33:51 -0700

    WIP build out and test input functionality

--- script.py
+++ script.py
@@ -14,16 +14,16 @@ class Player:
     self.decisions[self.current_scene] = decision
     self.current_scene += 1
 
+# Adventure class
 class Adventure:
   def __init__(self, player):
     self.player = player
     self.scenes = 5
-    self.current_scene = 0
+    self.current_scene = player.current_scene
   
   def play(self):
     while self.current_scene < self.scenes:
-      self.scenes[self.current_scene].play(self.player)
-      self.current_scene += 1
+      self.current_scene = self.player.current_scene
     if self.current_scene == self.scenes:
       self.end_game(self.player.decisions)
 
@@ -34,6 +34,30 @@ class Adventure:
       print("You win!")
     else:
       print("You lose!")
+
+# Scene dictionary
+scenes = {
+  0: "You come to the front door of an abandoned house. All of the lights are off. Do you (A) go inside or (B) go back home?",
+  1: "You are in a hallway. There is a door to your left and right, which one do you take?",
+  2: "You are in a kitchen. There is a door to your left and right, which one do you take?",
+  3: "You are in a bathroom. There is a door to your left and right, which one do you take?",
+  4: "You are in a bedroom. There is a door to your left and right, which one do you take?",
+  5: "You are in a living room. There is a door to your left and right, which one do you take?",
+}
+
+print("Please input your name and press enter to begin: ")
+player = Player(input())
+game = Adventure(player)
+# game.play()
+
+print("Welcome " + player.name + "!")
+# print(player.current_scene)
+print(scenes[player.current_scene])
+player.make_decision(input())
+if player.decisions[0] == ("B" or "b"):
+  print("You have your wits about you and decide to go back home. You live to see another day.")
+elif player.decisions[0] == ("A" or "a"):
+  print("You enter the abandoned house.")
 
 # mike = Player("Mike")
 # mike.make_decision("left")
